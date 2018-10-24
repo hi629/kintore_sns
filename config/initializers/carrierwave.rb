6eb485549f297ff0c39dc250780b2b9389c220ab
大平裕章 <hirojoy189@gmail.com>
2018-10-24 18:14:51 +0900

    upload photos to aws setting

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: 'AKIAJMGU2QQFK6GS5WNQ',
+        aws_secret_access_key: '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',
+        region: 'ap-northeast-1'
+    }
+
+    config.fog_directory = 'muscle-share'
+    config.cache_storage = :fog
+end
