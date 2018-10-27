b9adf3385e54b165efa1128b6a38964a1b7d8077
大平裕章 <hirojoy189@gmail.com>
2018-10-27 19:20:49 +0900

    en to ja

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   private
     def logged_in?
       !current_user.nil?
