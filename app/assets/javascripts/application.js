f1a58c34fe61f26761f3752a94c56d0c28fa254c
大平裕章 <hirojoy189@gmail.com>
2018-10-27 19:45:03 +0900

    statu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
       $(window).scroll(function() {
         var url = $('.pagination .next_page').attr('href');
         if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-          $('.pagination').text("Please Wait...");
+          $('.pagination').text("Loading...");
           return $.getScript(url);
         }
       });
