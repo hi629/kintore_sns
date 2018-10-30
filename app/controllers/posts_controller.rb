ca3424c2c0978087a9bfed97d0789c09121ae49c
大平裕章 <hirojoy189@gmail.com>
2018-10-30 16:18:25 +0900

    add devise secret_key, fix hash_tag page error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
     def hashtags
         @tag = Tag.find_by(name:params[:name])
-        @posts = @tag.posts
+        @posts = @tag.posts.order("created_at DESC").paginate(:page => params[:page], :per_page => 21)
     end
 
     def search
