86eef5b6fd7f185214b896bd705f1a0bee8dc377
大平裕章 <hirojoy189@gmail.com>
2018-10-30 17:06:04 +0900

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do |i|
+2.times do |i|
     User.create!(id:i+2, email:"test#{i}@test.com", name:"test#{i}", username:"test#{i}test#{i}", password:"12345678")
     10.times do |j|
         Post.create!(
